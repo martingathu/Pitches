c22b0592699b386a0fde16abb4401730c383764e
martingathu <martin5gathu@gmail.com>
2020-05-04 18:09:08 +0300

    add blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -34,10 +34,5 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
-    # Authentication blueprint
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
-
      
     return app
