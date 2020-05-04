33c76ebaab610115c00c3e86c78a84f01865c1b6
martingathu <martin5gathu@gmail.com>
2020-05-04 15:14:52 +0300

    as SECREAT KEY

---
+++ config.py
@@ -0,0 +1,44 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin:auntyalice55@localhost/pitches'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    # SECRET_KEY= os.environ.get("SECRET_KEY")
+    SECRET_KEY = '25616796'
+    
+    
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
